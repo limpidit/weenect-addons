048a76d9eb227ccb7c39e8f6f1c9f0f3873caba1
guillaumeLimpidIT <121024320+guillaumeLimpidIT@users.noreply.github.com>
2023-11-15 15:05:57 +0100

    Update inventory (#5)
    * Update vs files
    
    * Update
    
    * Update2
    
    * Update3
    
    * UPDATE3
    
    * UPDATE

--- products/models/product_template.py
+++ products/models/product_template.py
@@ -4,3 +4,5 @@ from odoo.exceptions import UserError
 class ProductTemplate(models.Model):
     _inherit = "product.template"
     
+    client_friendly_name=fields.Char(string="Friendly Name")
+    ean_weenect=fields.Char(string="EAN Weenect")
