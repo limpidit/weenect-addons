e7b9953f1596097934bf7520f00f5b294fde8934
guillaumeLimpidIT <121024320+guillaumeLimpidIT@users.noreply.github.com>
2023-11-15 14:21:06 +0100

    Preprod (#4)
    * [ADD] app ringover_dialer
    
    * Update inventory (#3)
    
    * [ADD] app ringover_dialer
    
    * Update vs files
    
    * Update
    
    * Update2
    
    * Update3

---
+++ products/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name":"Products",
+    "summary":"Products",
+    "version":"16.0",
+    "author":"Limpid IT",
+    "depends":[
+        "base",'web_studio', "crm", "sale"
+    ],
+    "installable":True,
+    "data":[
+        "views/product_template_views.xml",
+        "security/ir.model.access.csv",
+    ],
+    "license":"LGPL-3",
+}
