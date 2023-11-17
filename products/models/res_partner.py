ff1433c5cba37729a94287935351a2326f0af84a
guillaumeLimpidIT <guillaume.magnier@limpidit.com>
2023-11-17 15:37:54 +0100

    Ajout champs fiche contacts

--- products/models/res_partner.py
+++ products/models/res_partner.py
@@ -4,4 +4,11 @@ from odoo.exceptions import UserError
 class ResPartner(models.Model):
     _inherit = "res.partner"
     
+    num_retailer=fields.Char("Numéro Retailer")
     code_bic=fields.Char(string="Code BIC")
+    code_ape=fields.Char("Code APE")
+    date_ouverture=fields.Date("Date d'ouverture")
+    joom1_effectue=fields.Boolean("JOOM1 effectué")
+    derniere_activite_tl=fields.Date("Dernière activité TL")
+
+
