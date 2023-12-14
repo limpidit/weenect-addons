3aa0bd0a62db0ab3af88f31a067a4addbde7214c
guillaumeLimpidIT <guillaume.magnier@limpidit.com>
2023-12-14 12:06:03 +0100

    update

--- products/models/account_move.py
+++ products/models/account_move.py
@@ -5,10 +5,22 @@ class AccountMove(models.Model):
 
     tracking_numbers = fields.Char(string='Numéros de Tracking', compute='_compute_tracking_numbers')
 
-    @api.depends('invoice_line_ids.sale_line_ids.order_id.picking_ids')
+    @api.depends('invoice_line_ids')
     def _compute_tracking_numbers(self):
         for record in self:
-            pickings = record.mapped('invoice_line_ids.sale_line_ids.order_id.picking_ids')
-            tracking_numbers = ', '.join(pickings.mapped('tracking_number'))
-            record.tracking_numbers = tracking_numbers
+            tracking_numbers_set = set()
+            
+            for invoice_line in record.invoice_line_ids:
+                # Accéder aux lignes de commande de vente associées à chaque ligne de facture
+                sale_order_lines = invoice_line.sale_line_ids
+                for sale_order_line in sale_order_lines:
+                    # Accéder aux bons de livraison liés à chaque ligne de commande de vente
+                    pickings = sale_order_line.order_id.picking_ids
+                    for picking in pickings:
+                        if picking.tracking_number:
+                            tracking_numbers_set.add(picking.tracking_number)
+
+            record.tracking_numbers = ', '.join(tracking_numbers_set)
+
+
 
