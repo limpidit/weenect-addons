4a4265bc84ddecbf0987a043e3f68f56f5fd2b90
guillaumeLimpidIT <121024320+guillaumeLimpidIT@users.noreply.github.com>
2024-01-04 18:49:14 +0100

    Changement nom champs (#34) (#35)

--- products/models/sale_order.py
+++ products/models/sale_order.py
@@ -3,5 +3,5 @@ from odoo import models, fields, api
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    imei_filled = fields.Boolean(string='IMEI Renseignés',store=True)
+    imei_filled = fields.Boolean(string='IMEI enregistrés',store=True)
 
