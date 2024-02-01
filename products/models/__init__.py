ef7a3509ab48c4b9ab09220f0674a89bfda30fdc
guillaumeLimpidIT <guillaume.magnier@limpidit.com>
2024-02-01 14:41:06 +0100

    Pivot

--- products/models/__init__.py
+++ products/models/__init__.py
@@ -1,1 +1,1 @@
-from . import product_template, res_partner, traceurs_sav, stock_picking, account_move, sale_order
+from . import product_template, res_partner, traceurs_sav, stock_picking, account_move, sale_order, mail_thread, sale_order_line
