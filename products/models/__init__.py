4b8e5824e22ccc6a412dc3de7b2439ce7947bbc9
guillaumeLimpidIT <121024320+guillaumeLimpidIT@users.noreply.github.com>
2024-02-01 21:09:44 +0100

    01022024v3 (#45) (#46)
    * update pivot
    
    * Edit order date field label
    
    * Add language and country in pivot
    
    * e

--- products/models/__init__.py
+++ products/models/__init__.py
@@ -1,1 +1,1 @@
-from . import product_template, res_partner, traceurs_sav, stock_picking, account_move, sale_order
+from . import product_template, res_partner, traceurs_sav, stock_picking, account_move, sale_order, sale_order_line
