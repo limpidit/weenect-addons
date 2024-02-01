e56ee72d0855a10aa7ca6ed9bc82217856a2b3b0
guillaumeLimpidIT <guillaume.magnier@limpidit.com>
2024-02-01 21:01:07 +0100

    Add language and country in pivot

--- products/models/sale_order_line.py
+++ products/models/sale_order_line.py
@@ -5,4 +5,15 @@ class SaleOrderLine(models.Model):
 
     order_date = fields.Datetime(related='order_id.date_order', string='Date de la commande', store=True, readonly=True)
 
+    # Récupération du pays du client de la commande
+    customer_country_id = fields.Many2one(related='order_id.partner_id.country_id', string='Pays du client', store=True, readonly=True)
+
+    customer_language = fields.Char(compute='_compute_customer_language', string='Langue du client', store=True, readonly=True)
+
+    @api.depends('order_id.partner_id.lang')
+    def _compute_customer_language(self):
+        for record in self:
+            # Copier la valeur du champ lang du partenaire
+            record.customer_language = record.order_id.partner_id.lang
+
 
