09fa6094da470c599aa7a67eab05f38dff7f2259
guillaumeLimpidIT <guillaume.magnier@limpidit.com>
2024-02-01 20:53:41 +0100

    Edit order date field label

--- products/models/sale_order_line.py
+++ products/models/sale_order_line.py
@@ -3,6 +3,6 @@ from odoo import models, fields, api
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
-    order_date = fields.Datetime(related='order_id.date_order', string='Order Date', store=True, readonly=True)
+    order_date = fields.Datetime(related='order_id.date_order', string='Date de la commande', store=True, readonly=True)
 
 
