9889deaa2358059efd52b3803f4a3a5a46772297
guillaumeLimpidIT <121024320+guillaumeLimpidIT@users.noreply.github.com>
2024-03-19 18:32:32 +0100

    Update product_template.py

--- products/models/product_template.py
+++ products/models/product_template.py
@@ -4,5 +4,5 @@ from odoo.exceptions import UserError
 class ProductTemplate(models.Model):
     _inherit = "product.template"
     
-    client_friendly_name=fields.Char(string="Dénomination client")
+    client_friendly_name=fields.Char(string="Dénomination client", translate=True)
     ean_weenect=fields.Char(string="EAN Weenect")
