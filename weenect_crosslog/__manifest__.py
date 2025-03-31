003dc8268c9f93575462f5ed4a329308dec4cda4
Kameron Ramaherison <kameronramaherison@MacBook-Air-de-install.local>
2025-03-31 16:40:10 +0200

    Création interface module Crosslog

---
+++ weenect_crosslog/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    "name": "Crosslog Weenect LIT",
+    "summary": "Crosslog Weenect LIT",
+    "version": "16.0",
+    "author": "Limpid IT",
+    "depends": [
+        'base',
+        'product',
+        'stock'
+    ],
+    "data": [
+        # Security 
+        'security/ir.model.access.csv',
+        # Views
+        'views/menu.xml',
+    ],
+    "installable": True,
+    "application": True,
+    "license": "LGPL-3",    
+}
