0f4a5e7c71917c29c1f5920adbd8c9ab87b5cf35
Alexandre Gosselin <alexandre.gosselin@limpidit.com>
2025-11-18 11:43:42 +0100

    Migration weenect_edifact #1

--- weenect_edifact/models/edifact_invoic_d96a.py
+++
@@ -1,8 +0,0 @@
-
-
-class EdifactInvoicD96A:
-    def __init__(self):
-        self.segments = []
-
-    def get_unh_segment(self, id):
-        return ("UNH", id, ["INVOIC", "D", "96A", "UN", "EAN008"])
