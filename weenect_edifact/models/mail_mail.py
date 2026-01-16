a2a33b65974830fb8821cefbefe6f570f8d38c22
Kameron Ramaherison <kameronramaherison@MacBook-Air-de-install.local>
2026-01-16 16:32:47 +0100

    Fix erreur envoi mail

--- weenect_edifact/models/mail_mail.py
+++ weenect_edifact/models/mail_mail.py
@@ -7,7 +7,7 @@ import re
 class MailMail(models.Model):
     _inherit = 'mail.mail'
 
-    def _send(self, auto_commit=False, raise_exception=False, smtp_session=None):
+    def _send(self, auto_commit=False, raise_exception=False, smtp_session=None, **kwargs):
         # if self.body_html:
         #     self.body_html = re.sub('<[^<]+?>', '', self.body_html)
-        return super()._send(auto_commit=auto_commit, raise_exception=raise_exception, smtp_session=smtp_session)
+        return super()._send(auto_commit=auto_commit, raise_exception=raise_exception, smtp_session=smtp_session, **kwargs)
