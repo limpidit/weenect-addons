4038340bd5cc22f5e74d456de5b413e75460cd28
guillaumeLimpidIT <121024320+guillaumeLimpidIT@users.noreply.github.com>
2026-03-03 09:36:40 +0100

    Entrepots Amazon FBM (#103)
    Co-authored-by: Alexandre Gosselin <alexandre.gosselin@limpidit.com>

---
+++ weenect_amazon/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Weenect Amazon',
+    'version': '1.0',
+    'description': 'Affectation automatique de l\'entrepôt pour les commandes FBM Amazon selon l\'adresse de livraison.',
+    'author': 'Limpid IT',
+    'license': 'LGPL-3',
+    'depends': [
+        'sale_amazon',
+    ],
+    'data': [
+        'views/amazon_account_views.xml',
+    ],
+    'auto_install': False,
+    'application': False,
+}
